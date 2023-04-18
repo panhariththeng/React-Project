03e26df827bc6170429214be26cee0f57a4473ea
panhariththeng <102948199+panhariththeng@users.noreply.github.com>
2023-04-18 13:23:31 +0700

    first commit

---
+++ 09-color-generator/setup/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefaut()
+    try {
+      let colors = new Values(color).all(10)
+      console.log(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+    </>
+  )
+}
+
+export default App
